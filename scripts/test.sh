5dc3a380b10a32175946423d56fcebdfce56bc7d
pyshi <3088819680@qq.com>
2023-09-01 21:58:20 -0400

    minor changes

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,3 @@
-python transtouch/test.py --cfg configs/pengyang.yml -s TEST.WEIGHT @/model_060000.pth
+CUDA_VISIBLE_DEVICES=5 python transtouch/test.py --cfg configs/baseline1.yml -s TEST.WEIGHT /share/liuyu/useful_checkpoints/baseline.pth
+
+CUDA_VISIBLE_DEVICES=3 python transtouch/test.py --cfg configs/activezero2.yml -s TEST.WEIGHT /share/pengyang/pretrained/model_070000.pth
